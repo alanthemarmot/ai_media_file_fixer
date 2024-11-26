41c4780453b4ac41e5154148dcaaad1bd9866cdc
Alan Hendrick <github.com.empower628@passmail.com>
2024-11-26 12:53:41 +0000

    Initial commit

---
+++ aa_code_snippets/genai_snippets/openai_connect.py
@@ -0,0 +1,9 @@
+
+import os
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv(), override=True)
+
+os.environ.get("OPENAI_API_KEY")
+# os.environ.get("PINECONE_API_KEY")
+
+print("Hello!")
---
+++ aa_code_snippets/useful_shell.sh
@@ -0,0 +1,4 @@
+
+# Watch the disk usage of the current directory in human readable format
+ watch -n 1 'echo "Human Readable"; du -h --max-depth=2;echo ;echo "Non-Human Readable"; du --max-depth=2'T1
+
